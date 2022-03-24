e359c9e9d6ae18932a1e217e799e0ebd671e2597
Erol Yarataner <eyarataner@gmail.com>
2022-03-24 16:12:08 +0100

    admin and model files for dataitem were updated

--- oetechproject/dataitem/models.py
+++ oetechproject/dataitem/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class DataItem(models.Model):
+    item_id = models.IntegerField(default=0)
+    reference = models.CharField(max_length=10)
+    name = models.CharField(max_length=10)
+    owner = models.CharField(max_length=100)
+    #any necessary information field can be added based on CSV file - due to the time limitation, only those parameters were created
+
+    def __str__(self) -> str:
+        return f"{self.reference} - {self.name}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
