52f227fa256d3147cbb988e487197abd0d4ac31d
Erol Yarataner <eyarataner@gmail.com>
2022-03-24 16:19:31 +0100

    views file was updated to be able to upload the csv document

--- oetechproject/csvapp/views.py
+++ oetechproject/csvapp/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from .forms import CsvModelForm
+from .models import Csv
+import csv
+from dataitem.models import DataItem
 
 # Create your views here.
+
+def upload_file_view(request):
+    form = CsvModelForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        form = CsvModelForm()
+        obj = Csv.objects.get(activated=False)
+        with open(obj.file_name.path, 'r') as f:
+            reader = csv.reader(f)
+            for i, row in enumerate(reader):
+                if i == 0: # To get rid of header row
+                    pass
+                else:
+                    a = row[0]
+                    item_id = a.split(",")[0]
+                    reference = a.split(",")[1]
+                    name = a.split(",")[2]
+                    owner = a.split(",")[3]
+                    DataItem.objects.create(
+                        item_id = item_id,
+                        reference = reference,
+                        name = name,
+                        owner = owner
+                    )
+
+            obj.activated = True
+            obj.save()
+    dataitems = DataItem.objects.all()
+
+    return render(request, 'csvs/upload.html', {
+        'form':form,
+        'dataitems':dataitems
+    })
